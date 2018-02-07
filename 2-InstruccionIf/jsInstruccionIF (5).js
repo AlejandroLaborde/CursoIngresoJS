70a9291b82a02e0bd31474989acc3b5a4a527dab
Alejandro Laborde Parodi <36174258+AlejandroLaborde@users.noreply.github.com>
2018-02-06 22:03:59 -0300

    ejercicios resueltos de 1 al 6

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+edad = document.getElementById('edad').value;
 
+if(edad>12 && edad<=17)
+{
+
+}
+else
+{
+	alert("no es adolecente");
+}
+
+	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,25 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+	edad = document.getElementById('edad').value;
+
+	
+	if(edad>12 && edad<=17)
+	{
+		alert("esadolecente");
+	}
+	else
+	{
+		if(edad<13)
+		{
+		alert("es menor de edad");
+		}
+		else
+		{
+		alert("es mayor de edad");
+		}	
+	}
+}
 
 
-
-
-}//FIN DE LA FUNCIÓN
