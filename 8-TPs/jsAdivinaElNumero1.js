61777ae1e9bce39c100e3226257dbfc8f3754d3e
Alejandro Laborde Parodi <36174258+AlejandroLaborde@users.noreply.github.com>
2018-02-18 20:07:26 -0300

    nose porque no me funciona

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -7,20 +7,28 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
 var contadorIntentos;
+var numeroRandom;
+var numeroSecreto;
+numeroRandom=Math.floor((Math.random() *100)+1);
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
 	
-
+	console.log(numeroRandom);
 }
-
+	
 function verificar()
-{
-	
-	
+{	
+	numeroSecreto=document.getElementById('numero').value;
+
+	switch (numeroSecreto)
+	{
+		case numeroRandom :
+			alert("ganaste");
+		break;
+	}
+	alert("sale del switch");
+
+
 }
